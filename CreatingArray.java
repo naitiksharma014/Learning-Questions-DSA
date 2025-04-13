abb360294b718af537a711f06ed88200b45ac30b
naitiksharma014 <naitiksharma014@gmail.com>
2025-04-13 20:10:42 +0530

    Add files via upload

---
+++ CreatingArray.java
@@ -0,0 +1,19 @@
+package ARRAY;
+import java.util.*;
+
+//Syntax
+//dataType arrayName[] = new dataType[size];
+
+public class CreatingArray {
+    public static void main(String[] args) {
+        int marks[] = new int[50];
+        int num[] = {1,2,3,4};
+        String fruits[] = {"apple","mango","orange"};
+
+        System.out.println(num);
+        System.out.println(fruits);
+    }
+
+
+    
+}
